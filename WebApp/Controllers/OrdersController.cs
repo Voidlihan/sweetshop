7e7866bdd66a9f3dbe4bae5ebffaec5f09f0a1e9
e.alihangt@gmail.com <e.alihangt@gmail.com>
2020-07-21 18:25:40 +0600

    controllers, uow

--- WebApp/Controllers/OrdersController.cs
+++ WebApp/Controllers/OrdersController.cs
@@ -14,104 +14,11 @@ namespace WebApp.Controllers
 {
     public class OrdersController : Controller
     {
-        private SweetShopDataContext context = new SweetShopDataContext();
-
-        public ActionResult Index() => View(context.Orders.Include(o => o.Customer).ToList());
-
-        public ActionResult Details(int? id)
+        private readonly EFUnitOfWork _uow;
+        public OrdersController()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = context.Orders.Include(o => o.Customer).FirstOrDefault(s => s.Id == id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            return View(order);
+            _uow = new EFUnitOfWork();
         }
-
-        public ActionResult Create()
-        {
-            ViewBag.CustomerId = new SelectList(context.Customers, "Id", "Name");
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,CustomerId,TotalPrice")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                context.Orders.Add(order);
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CustomerId = new SelectList(context.Customers, "Id", "Name", order.CustomerId);
-            return View(order);
-        }
-
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = context.Orders.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.CustomerId = new SelectList(context.Customers, "Id", "Name", order.CustomerId);
-            return View(order);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,CustomerId,TotalPrice")] Order order)
-        {
-            if (ModelState.IsValid)
-            {
-                context.Entry(order).State = EntityState.Modified;
-                context.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.CustomerId = new SelectList(context.Customers, "Id", "Name", order.CustomerId);
-            return View(order);
-        }
-
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Order order = context.Orders.Find(id);
-            if (order == null)
-            {
-                return HttpNotFound();
-            }
-            return View(order);
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Order order = context.Orders.Find(id);
-            context.Orders.Remove(order);
-            context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                context.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+        public ActionResult Index() => View(_uow.Orders.GetAll());
     }
 }
